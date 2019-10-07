b18af46732d34e9a983fe6d12c2c190f57939a2b
kjanghala <spo.kanika@gmail.com>
2019-10-06 20:04:11 -0700

    filtering data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,20 @@
-// from data.js
-var tableData = data;
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+data.forEach((UFOsightings) => {
+  var row = tbody.append("tr");
+  Object.entries(UFOsightings).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  })
+});
+
+var sightings = data;
+var button = d3.select("#button");
+
+button.on("click", function() {
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property("value");
+  var filteredData = sightings.filter(sighting => sighting.datetime === inputValue);
+  console.log(filteredData);
+})
+
